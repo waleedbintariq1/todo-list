2d0d2f080950c31960dc789c669250a94f5e4c38
Waleed Bin Tariq <waleed.b.tariq@gmail.com>
2021-10-04 14:52:34 +0500

    Displayed Date and Time

--- src/My Components/Todo.jsx
+++ src/My Components/Todo.jsx
@@ -4,16 +4,44 @@ import "../My CSS/Todo.css";
 export default function Todo(props) {
   const { todo, index } = props;
 
+  // createdAt returned from the server is a string
+  // so, we first get the milliseconds using Date.parse()
+  // then, we pass milliseconds to Date's constructor
+  const properDate = new Date(Date.parse(todo.createdAt));
+
+  const day = properDate.getDate();
+  const month = properDate.getMonth() + 1;
+  const year = properDate.getFullYear();
+  let hours = properDate.getHours();
+  const minutes = properDate.getMinutes();
+
+  let amOrPm = "";
+
+  if (hours >= 12) {
+    hours -= 12;
+    amOrPm = "pm";
+  } else {
+    amOrPm = "am";
+  }
+
   return (
     <div className="mb-3 p-3 todoBorder" key={index}>
       <div className="container">
-        <div className="contentOuterLayout">
-          <div className="contentInnerLayout">
+        <div className="contentLayout">
+          <div className="textLayout">
             <h5 className="itemText">{todo.desc}</h5>
-            <h5>{todo.createdAt}</h5>
+
+            <div className="dateAndTime">
+              <div className="time">
+                {hours}:{minutes} {amOrPm}
+              </div>
+              <div className="date">
+                {day}/{month}/{year}
+              </div>
+            </div>
           </div>
 
-          <div className="itemButtons">
+          <div className="buttonsLayout">
             <button
               className="btn btn-outline-primary"
               data-toggle="modal"
@@ -22,7 +50,7 @@ export default function Todo(props) {
                 props.onEdit(index);
               }}
             >
-              E
+              Edit
             </button>
             <button
               className="btn btn-outline-danger"
